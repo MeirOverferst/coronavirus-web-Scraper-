bcf2bfc3fa525a0f21ebb63d37c62c15f7c954cf
MeirOverferst <meiroverferst@gmail.com>
2020-03-21 00:37:47 +0100

    new approach done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-
+    const _ = require('lodash');
     let worldometersUrl = "https://www.worldometers.info/coronavirus/";
 
     let browser = await puppeteer.launch();
@@ -14,9 +14,9 @@ const puppeteer = require("puppeteer");
     });
 
     const data = await page.evaluate(() => {
-        //    const scrape = Array.from(Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(1)'))) ;
+        /// 
         let sorting = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > .sorting'));
-       
+        ///
         let cNames = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(1)'));
         let tCases = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(2)'));
         let nCases = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(3)'));
@@ -24,6 +24,8 @@ const puppeteer = require("puppeteer");
         let nDeaths = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(5)'));
         let rRecov = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(6)'));
         let aCases = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(7)'));
+        let Sccases = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(8)'));
+        let tCasesPerMil = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(9)'));
 
         let sortingOrg = sorting.map(td => td.innerText);
         let cNamesOrg = cNames.map(td => td.innerText);
@@ -33,9 +35,8 @@ const puppeteer = require("puppeteer");
         let nDeathsOrg = nDeaths.map(td => td.innerText);
         let rRecovOrg = rRecov.map(td => td.innerText);
         let aCasesOrg = aCases.map(td => td.innerText);
-
-
-
+        let SccasesOrg = Sccases.map(td => td.innerText);
+        let tCasesPerMilOrg = tCasesPerMil.map(td => td.innerText);
 
         return {
             sortingOrg,
@@ -45,114 +46,28 @@ const puppeteer = require("puppeteer");
             tDeathsOrg,
             nDeathsOrg,
             rRecovOrg,
-            aCasesOrg
+            aCasesOrg,
+            SccasesOrg,
+            tCasesPerMilOrg
         }
 
     })
+    let arr1 = data.cNamesOrg;
+    let arr2 = data.tCasesOrg;
+    let arr3 = data.nCasesOrg;
+    let arr4 = data.tDeathsOrg;
+    let arr5 = data.nDeathsOrg;
+    let arr6 = data.rRecovOrg;
+    let arr7 = data.aCasesOrg;
+    let arr8 = data.SccasesOrg;
+    let arr9 = data.tCasesPerMilOrg;
 
-    console.log("data.sortingOrg",data.sortingOrg);
-    console.log("data.cNamesOrg",data.cNamesOrg);
-    console.log("data.tCasesOrg",data.tCasesOrg);
-    console.log("data.nCasesOrg",data.nCasesOrg);
-    console.log("data.tDeathsOrg",data.tDeathsOrg);
-    console.log("data.nDeathsOrg",data.nDeathsOrg);
-    console.log("data.rRecovOrg",data.rRecovOrg);
-    console.log("data.aCasesOrg",data.aCasesOrg);
-
-    console.log("data.cNamesOrg.length)",data.cNamesOrg.length);
-    console.log("data.tCasesOrg.length)",data.tCasesOrg.length);
-    console.log("data.nCasesOrg.length)",data.nCasesOrg.length);
-    console.log("data.tDeathsOrg.length)",data.tDeathsOrg.length);
-    console.log("data.nDeathsOrg.length)",data.nDeathsOrg.length);
-    console.log("data.rRecovOrg.length)",data.rRecovOrg.length);
-    console.log("data.aCasesOrg.length)",data.aCasesOrg.length);
-
-
-
+    
+const result = _.map(
+    _.zip(arr1,arr2,arr3,arr4,arr5,arr6,arr7,arr8,arr9),
+    _.partial(_.zipObject, ["Country/Other", "Total Cases","New Cases","Total Deaths","Total Recovered","Active Cases","Serious,Critical","Total Cases per Million"])
+  )
+  
+  console.log(result)
 
 })();
-
-
-//const puppeteer = require("puppeteer");
-
-// (async () => {
-
-//     let worldometersUrl = "https://www.worldometers.info/coronavirus/";
-
-//     let browser = await puppeteer.launch();
-//     let page = await browser.newPage();
-
-//     await page.goto(worldometersUrl, {
-//         waitUntil: "networkidle2"
-//     });
-
-//     let data = await page.evaluate(() => {
-//         let cNames = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(1)'));
-//         let tCases = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(2)'));
-//         let nCases = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(3)'));
-//         let tDeaths = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(4)'));
-//         let nDeaths = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(5)'));
-//         let rRecov = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(6)'));
-//         let aCases = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(7)'));
-//         return {
-//             cNames,
-//             tCases,
-//             nCases,
-//             tDeaths,
-//             nDeaths,
-//             rRecov,
-//             aCases
-//         }
-//     })
-
-
-
-// ///scraping counties names and printing
-// let cNamesArr= [ ];
-// for (let i = 0; i < data.cNames.length; i++) {
-// cNamesArr.push(data.cNames[i].textContent);
-// }
-// console.log(cNamesArr);
-
-
-// ///scraping total cases and printing
-// let tCasesArr = [ ];
-// for (let i = 0; i < data.tCases.length; i++) {
-//     tCasesArr.push(data.tCases[i].textContent);
-//     }
-//     console.log(tCasesArr);
-// ///scraping new cases and printing
-// let nCasesArr = [ ];
-// for (let i = 0; i < data.nCases.length; i++) {
-//     nCasesArr.push(data.nCases[i].textContent);
-//     }
-//     console.log(nCasesArr);
-
-// ///scraping total deaths and printing
-//     let tDeathsArr= [ ];
-//     for (let i = 0; i < data.tDeaths.length; i++) {
-//         tDeathsArr.push(data.tDeaths[i].textContent);
-//     }
-//     console.log(tDeathsArr);
-//  ///scraping new deaths and printing
-// let nDeathsArr= [ ];
-// for (let i = 0; i < data.nDeaths.length; i++) {
-//     nDeathsArr.push(data.nDeaths[i].textContent);
-// }
-// console.log(nDeathsArr);
-
-//  ///scraping total recovers and printing
-//  let rRecovArr= [ ];
-//  for (let i = 0; i < data.rRecov.length; i++) {
-//     rRecovArr.push(data.rRecov[i].textContent);
-//  }
-//  console.log(rRecovArr);
-
-//  ///scraping active cases and printing
-// let aCasesArr = [ ];
-// for (let i = 0; i < data.aCases.length; i++) {
-//     aCasesArr.push(data.aCases[i].textContent);
-//     }
-//     console.log(aCasesArr);
-
-// })();
