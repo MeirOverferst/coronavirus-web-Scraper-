552b96ade5cb6a3a3206c916d652d9a7d9383885
MeirOverferst <meiroverferst@gmail.com>
2020-03-24 14:04:44 +0100

    first

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-let express =require("express");
-let app = express();
-let PORT = process.env.PORT || 5000;
+const express =require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
 const router = express.Router();
 app.use("/api",router)
 let fs = require('fs');
@@ -8,19 +8,22 @@ const puppeteer = require("puppeteer");
 process.setMaxListeners(Infinity);
 var cron = require('node-cron');
 
-cron.schedule('*/11* * * *', () => {
-console.log('running a task every 30 minutes');
+cron.schedule('*/10 * * * *', () => {
+console.log('running a task every 10 minutes');
 
  (async () => {
         const _ = require('lodash');
         let worldometersUrl = "https://www.worldometers.info/coronavirus/";
-    
-        const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
 
-        let page = await browser.newPage();
-        await page.goto(worldometersUrl, {
-            waitUntil: "networkidle2"
+        const browser = await puppeteer.launch({
+          args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+          ],
         });
+        let page = await browser.newPage();
+        await page.goto(worldometersUrl, { waitUntil: "networkidle2"});
+
         console.log("puppeteer is on");
 
         const data = await page.evaluate(() => {
@@ -36,7 +39,7 @@ console.log('running a task every 30 minutes');
             let aCases = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(7)'));
             let Sccases = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(8)'));
             let tCasesPerMil = Array.from(document.querySelectorAll('#main_table_countries_today [role="row"] > td:nth-child(9)'));
-    
+
             let sortingOrg = sorting.map(td => td.innerText);
             let cNamesOrg = cNames.map(td => td.innerText);
             let tCasesOrg = tCases.map(td => td.innerText);
@@ -61,6 +64,7 @@ console.log('running a task every 30 minutes');
                 tCasesPerMilOrg
             }
         })
+
         let arr1 = data.cNamesOrg;
         let arr2 = data.tCasesOrg;
         let arr3 = data.nCasesOrg;
@@ -71,15 +75,14 @@ console.log('running a task every 30 minutes');
         let arr8 = data.SccasesOrg;
         let arr9 = data.tCasesPerMilOrg;
     
-        
-      const result = _.map(
+        const result = _.map(
         _.zip(arr1,arr2,arr3,arr4,arr5,arr6,arr7,arr8,arr9),
         _.partial(_.zipObject, ["Country/Other", "Total Cases","New Cases","Total Deaths","New Deaths","Total Recovered","Active Cases","Serious,Critical","Total Cases per Million"])
-      )
-      console.log( "got the results");
+        )
+       console.log( "got the results");
 
-     fs.writeFile('./db/updateData.json',JSON.stringify(result), function (err) {
-        if (err) return console.log(err);
+       fs.writeFile('./db/updateData.json',JSON.stringify(result), function (err) {
+       if (err) return console.log(err);
         console.log('results > updateData.json');
       });
       console.log('done writing');
@@ -89,16 +92,6 @@ console.log('running a task every 30 minutes');
 
   });
 
-
-
-
-
-
-
-// router.get("/", (req,res)=>{
-//   let uData = require('./db/updateData.json');
-//   res.json(uData);
-// });
 
 router.get('/', (req, res) => {
 
